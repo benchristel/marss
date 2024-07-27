da492943e70fe36821b1fd9a5fe58433f70f7115
Ben Christel <ben.christel@gmail.com>
2024-07-27 13:34:43 -0700

    Pass first real system test

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,5 @@
-const message: string = "Hello, world!"
-console.log(message)
+import {writeFileSync} from "fs"
+
+const args = process.argv.slice(2)
+const outputPath = args[1]
+writeFileSync(outputPath, `<rss version="2.0">`, "utf-8")
--- system-tests/marss.test.ts
+++ system-tests/marss.test.ts
@@ -1,18 +1,33 @@
-import {test, expect, is} from "@benchristel/taste"
+import {test, expect, is, trimMargin} from "@benchristel/taste"
 import {Process, spawnSync} from "../src/platform/subprocess"
+import {existsSync, mkdtempSync, readFileSync, writeFileSync} from "fs"
 
-function marss(): Process {
-    return spawnSync("node", "dist/main.js")
+function marss(...args: string[]): Process {
+    return spawnSync("node", "dist/main.js", ...args)
 }
 
 test("marss", {
-    "runs successfully"() {
-        const {success} = marss()
-        expect(success, is, true)
-    },
+    "writes the requested RSS file"() {
+        const input = trimMargin`
+            <!--
+            @marss
+            title: A Cool Blog
+            link: https://example.com
+            description: this is the description
+            -->`
+        const dir = mkdtempSync("/tmp/marss-")
+        writeFileSync(`${dir}/input.md`, input, "utf-8")
 
-    "doesn't log to stderr"() {
-        const {stderr} = marss()
+        const {success, stderr} = marss(`${dir}/input.md`, `${dir}/output.rss`)
+
+        expect(success, is, true)
         expect(stderr.toString(), is, "")
+        expect(existsSync(`${dir}/output.rss`), is, true)
+        const feed = readFileSync(`${dir}/output.rss`, "utf-8")
+        expect(feed, contains, `<rss version="2.0">`)
     },
 })
+
+function contains(needle: string, haystack: string): boolean {
+    return haystack.includes(needle)
+}
